a086ea3ef97f9d927d53dbf92a8582334fa1d289
JunJie957 <johnathan1874@163.com>
2020-08-11 22:59:09 +0800

    修改66题算法

--- 66. Plus One.cpp
+++ 66. Plus One.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        int len = digits.size();
-        int v = digits[len - 1];
-        v += 1;
-        if (v < 10) digits[len - 1] = v;
-        else
-        {
-            digits[len - 1] = v % 10;
-            for (int i = len - 2; i >= 0; --i)
-            {
-                v = digits[i] + v / 10;
-                if (v < 10)
-                {
-                    digits[i] = v;
-                    break;
-                }
-                else digits[i] = v % 10;
-                    
+        
+        for (int i = digits.size() - 1; i >= 0; --i) {
+            if (++digits[i] >= 10) {
+                digits[i] %= 10;
+                continue;
             }
-            if (v >= 10) digits.insert(digits.begin(), v / 10);
+            else {
+                break;
+            }
         }
+
+        if (digits[0] == 0) {
+            digits.insert(digits.begin(), 1);
+        }
+
         return digits;
     }
 };
