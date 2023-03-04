d67a7b944640fd677570b6892ada2c0efd711c59
wangjunjie <wangjunjie@ishumei.com>
2023-03-04 10:50:54 +0800

    fix

--- 70. Climbing Stairs.cpp
+++ 70. Climbing Stairs.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        int a = 0, b = 1, c = 0;
-        while (n--)
-        {
-            c = a + b;  // 第 n 层状态 = 第 n - 1 层状态 + 第 n - 2 层状态
-            a = b;      // 第 n - 2 层状态，相对于 n + 1 层来说
-            b = c;      // 第 n - 1 层状态，相对于 n + 1 层来说
+        if (n <= 2) return n;
+        int f1 = 1, f2 = 2, f3 = 3;
+        for (int i = 3; i <= n; ++i) {
+            f3 = f2 + f1;
+            f1 = f2;
+            f2 = f3;
         }
-        return c;
+        return f3;
     }
 };
